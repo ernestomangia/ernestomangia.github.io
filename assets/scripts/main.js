ba79e7b4630468e335a8da68e87a3d5a2cf0e908
Ernesto <ernesto_mangia@hotmail.com>
2022-08-30 19:56:27 -0300

    Refactored main.js Added missing title attr Minor improvements to mobile styles Removed text-justify

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,42 +1,46 @@
 class Site {
     constructor() {
+        this.year = new Date().getFullYear();
+    }
+
+    init() {
+        this.bindEvents();
+        this.populateCurrentYear();
+    }
+
+    bindEvents() {
+        this.bindModalEvents();
+    }
+
+    bindModalEvents() {
+        const projectModal = document.getElementById('projects-modal');
+
+        projectModal.addEventListener('show.bs.modal', event => {
+            const clickedButton = event.relatedTarget;
+            const selectedProject = clickedButton.getAttribute('data-bs-project');
+
+            // Remove old active
+            const tabLinks = projectModal.getElementsByClassName('nav-link');
+            const tabs = projectModal.getElementsByClassName('tab-pane');
+
+            Array.from(tabLinks).forEach(element => {
+                element.classList.remove("active");
+            });
+
+            Array.from(tabs).forEach(element => {
+                element.classList.remove("active", "show");
+            });
+
+            // Set new active
+            const tabLink = projectModal.querySelector('#nav-' + selectedProject + "-tab");
+            const tab = projectModal.querySelector('#nav-' + selectedProject);
+
+            tabLink.classList.add("active");
+            tab.classList.add("show", "active");
+        });
+    }
+
+    populateCurrentYear() {
+        document.getElementById("current-year").innerHTML = this.year;
     }
 }
-
-Site.fn = Site.prototype;
-
-Site.fn.init = function () {
-    this.bindEvents();
-};
-
-Site.fn.bindEvents = function () {
-    this.bindModalEvents();
-};
-
-Site.fn.bindModalEvents = function () {
-    const projectModal = document.getElementById('projects-modal');
-
-    projectModal.addEventListener('show.bs.modal', event => {
-        const clickedButton = event.relatedTarget;
-        const selectedProject = clickedButton.getAttribute('data-bs-project');
-
-        // Remove old active
-        const tabLinks = projectModal.getElementsByClassName('nav-link');
-        const tabs = projectModal.getElementsByClassName('tab-pane');
-
-        Array.from(tabLinks).forEach(element => {
-            element.classList.remove("active");
-        });
-
-        Array.from(tabs).forEach(element => {
-            element.classList.remove("active", "show");
-        });
-
-        // Set new active
-        const tabLink = projectModal.querySelector('#nav-' + selectedProject + "-tab");
-        const tab = projectModal.querySelector('#nav-' + selectedProject);
-
-        tabLink.classList.add("active");
-        tab.classList.add("show", "active");
-    });
-};
